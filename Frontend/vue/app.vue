8d099fa29f5169f7851f9bb4d41a92806faf85ea
hem436 <hemant436268s@gmail.com>
2022-08-11 01:45:45 +0530

    app.vue

---
+++ Frontend/vue/app.vue
@@ -0,0 +1,23 @@
+<script>
+export default{
+    data(){
+        return {
+            my:{
+                name:"S.M.Hemant",
+                lastlogtime:13
+            }
+        }
+    }
+}
+</script>
+
+<template>
+    <p class="w">My first vue application {{ my }}</p>
+</template>
+
+<style>
+.w{
+    color:red;
+    font-weight: bold;
+}
+</style>
